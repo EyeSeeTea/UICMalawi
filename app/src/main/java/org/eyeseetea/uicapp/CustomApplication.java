712b5e6fbef2dd1161b65a7f06537dcf7897e319
arturomejiamarmol <arturomejiamarmol@gmail.com>
2018-03-07 08:15:31 -0400

    Much performance way to store sensitive data using gradle.properties More info https://stackoverflow.com/questions/12749225/where-to-put-gradle-configuration-i-e-credentials-that-should-not-be-committe

--- app/src/main/java/org/eyeseetea/uicapp/CustomApplication.java
+++ app/src/main/java/org/eyeseetea/uicapp/CustomApplication.java
@@ -2,22 +2,16 @@ package org.eyeseetea.uicapp;
 
 
 import android.app.Application;
-import android.support.annotation.NonNull;
 
 import com.github.stkent.bugshaker.BugShaker;
 import com.github.stkent.bugshaker.flow.dialog.AlertDialogType;
 import com.github.stkent.bugshaker.github.GitHubConfiguration;
-
-import org.eyeseetea.uicapp.credentials.CredentialsRepository;
-import org.eyeseetea.uicapp.credentials.RawCredentialsRepository;
 
 public class CustomApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-        String getGitHubBotToken = getGitHubBotToken();
-
 
         BugShaker.get(this)
                 .setEmailAddresses("someone@example.com")
@@ -25,16 +19,11 @@ public class CustomApplication extends Application {
                 .setAlertDialogType(AlertDialogType.APP_COMPAT)
                 .setGitHubInfo(new GitHubConfiguration(
                         "eyeseetea/bugshaker-android",
-                        getGitHubBotToken,
+                        BuildConfig.GIT_HUB_BOT_TOKEN,
                         "eyeseeteabottest/snapshots",
                         "master"))
                 .assemble()
                 .start();
     }
 
-    @NonNull
-    private String getGitHubBotToken() {
-        CredentialsRepository credentials = new RawCredentialsRepository(this);
-        return credentials.getGitHubBotToken();
-    }
 }
